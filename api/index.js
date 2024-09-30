15d4b2a1c2b1dfb259ddcbc9d3ca05bcdb5aab96
tonandton <www.sorasakok@gmail.com>
2024-09-30 19:40:48 +0700

    create signup

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
 mongoose
   .connect(
-    "mongodb+srv://tonandton193:GTgMUNR80CXnV5Ci@mern-estate.y7r0a.mongodb.net/?retryWrites=true&w=majority&appName=mern-estate"
+    "mongodb+srv://tonandton193:GTgMUNR80CXnV5Ci@mern-estate.y7r0a.mongodb.net/mern-estate?retryWrites=true&w=majority&appName=mern-estate"
   )
   .then(() => {
     console.log("Connected to MongoDB!");
@@ -17,6 +19,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
